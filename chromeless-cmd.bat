ee8c43e99830c91e61ee542d5c23a865da06e317
Yonggang Luo <luoyonggang@gmail.com>
2015-03-18 17:53:24 +0800

    Rename to shell-sdk, do not use uuid.

--- chromeless-cmd.bat
+++ chromeless-cmd.bat
@@ -1,3 +1,4 @@
+call node %~dp0bin\gecko.js %*
 echo > "%~dp0build\Chromeless API Demo\.purgecaches"
 call "%~dp0build\Chromeless API Demo\Chromeless API Demo.exe" -console -jsconsole -purgecaches
 pause
--- chromeless-run.bat
+++ chromeless-run.bat
@@ -1,3 +1,3 @@
 echo > "%~dp0build\Chromeless API Demo\.purgecaches"
-call "%~dp0build\Chromeless API Demo\Chromeless API Demo.exe" -console -jsconsole -purgecaches
+"%~dp0build\Chromeless API Demo\Chromeless API Demo.exe" -console -jsconsole -purgecaches
 pause
